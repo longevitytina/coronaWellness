8547b7ad12b3f9909499c9a5881149ad7e81d420
Tina <longevity.tina@gmail.com>
2020-03-27 14:52:07 -0700

    adds submit listener

--- public/scripts/solutionsEdit.js
+++ public/scripts/solutionsEdit.js
@@ -2,6 +2,7 @@ console.log("connected solutionsIndex JS")
 console.log("sanity check")
 const API_BASE = "/api"
 const solutions = document.getElementById("solutions")
+const solutionForm = document.getElementById("editSolution")
 const solutionId = window.location.pathname.split("/")[4]
 const problemId = window.location.pathname.split("/")[2]
 
@@ -11,14 +12,7 @@ fetch(`${API_BASE}/problems/${problemId}/solutions/${solutionId}`)
   .then((res) => render(res))
   .catch((err) => console.log(err))
 
-// function render(solutionsArray) {
-//   console.log("Rendering update form", solutionsArray)
-//   const problemTemplate = solutionsArray
-//     .map((problem) => updateForm(problem))
-//     .join("")
-//   solutions.insertAdjacentHTML("beforeend", problemTemplate)
-// }
-
+// update form values
 function updateForm(solution) {
   console.log("solution = ", solution)
   const nameInput = document.getElementById("name")
@@ -31,3 +25,36 @@ function updateForm(solution) {
   imageInput.value = solution.image
   linkInput.value = solution.link
 }
+
+// Listen/Handle New Post Submit
+solutionForm.addEventListener("save", (event) => {
+  event.preventDefault()
+
+  const name = document.getElementById("name")
+  const description = document.getElementById("description")
+  const image = document.getElementById("image")
+  const link = document.getElementById("link")
+
+  const updatedSolution = {
+    name: name.value,
+    description: description.value,
+    image: image.value,
+    link: link.value,
+  }
+  console.log("submit", updatedSolution)
+
+  // REDIRECT TO PROBLEM PAGE
+  fetch(`${API_BASE}/problems/${problemId}/solutions/${solutionId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedSolution),
+  })
+    .then((stream) => stream.json())
+    .then((res) => {
+      console.log(res)
+      window.location = `/problems/${problemId}`
+    })
+    .catch((err) => console.log(err))
+})
