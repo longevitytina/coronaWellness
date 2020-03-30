623a85542d0776b3b8c00683ba5de9edccefefaa
Tina <longevity.tina@gmail.com>
2020-03-29 17:38:35 -0700

    links solutions to problems

--- seed.js
+++ seed.js
@@ -12,51 +12,60 @@ const problems = [
     solutionNames: ["Energy purification", "Crystal"],
   },
   {
-    name: "Back pain",
-    description:
-      "Physical discomfort occurring anywhere on the spine or back, ranging from mild to disabling.",
-    image: "../images/fatigue.jpg",
-    solutions: [],
+    properties: {
+      name: "Back pain",
+      description:
+        "Physical discomfort occurring anywhere on the spine or back, ranging from mild to disabling.",
+      image: "../images/fatigue.jpg",
+      solutions: [],
+    },
+    solutionNames: ["Meditation", "Stretching"],
   },
   {
-    name: "Anxiety",
-    description:
-      "A mental health disorder characterized by feelings of worry, anxiety, or fear that are strong enough to interfere with ones daily activities.",
-    image: "../images/anxiety.png",
-    solutions: [],
+    properties: {
+      name: "Anxiety",
+      description:
+        "A mental health disorder characterized by feelings of worry, anxiety, or fear that are strong enough to interfere with ones daily activities.",
+      image: "../images/anxiety.png",
+      solutions: [],
+    },
+    solutionNames: ["Meditation", "Energy purification"],
   },
   {
-    name: "Depression",
-    description:
-      "A mental health disorder characterized by persistently depressed mood or loss of interest in activities, causing significant impairment in daily life.",
-    image: "../images/depression.png",
-    solutions: [],
+    properties: {
+      name: "Depression",
+      description:
+        "A mental health disorder characterized by persistently depressed mood or loss of interest in activities, causing significant impairment in daily life.",
+      image: "../images/depression.png",
+      solutions: [],
+    },
+    solutionNames: ["Crystal", "Stretching"],
   },
   {
-    name: "Stress",
-    description: "Stress is a feeling of emotional or physical tension.",
-    image: "../images/stress.jpg",
-    solutions: [],
+    properties: {
+      name: "Stress",
+      description: "Stress is a feeling of emotional or physical tension.",
+      image: "../images/stress.jpg",
+      solutions: [],
+    },
+    solutionNames: ["Meditation", "Crystal"],
   },
 ]
 
 const solutions = [
   {
-    id: "1",
     name: "Crystal",
     description: "This white crystal is considered a “master healer.",
     image: "../images/crystals.jpg",
     link: "https://www.energymuse.com/crystal-shop",
   },
   {
-    // id: "2",
     name: "Stretching",
     description: "keeps the muscles flexible, strong, and healthy ",
     image: "../images/stretch.jpg",
     link: "https://www.youtube.com/watch?v=Ji9vKgfWK88",
   },
   {
-    // id: "3",
     name: "Meditation",
     description: "It's about training in awareness",
     image: "../images/meditation.jpg",
@@ -71,50 +80,23 @@ const solutions = [
   },
 ]
 
-// console.log("Deleting all problems...")
-// db.Problem.deleteMany({})
-//   .then((results) => {
-//     console.log(`Successfully deleted ${results.deletedCount} problems.`)
-//     return db.Problem.create(problems)
-//   })
-//   .then((newProblems) => {
-//     console.log(`Successfully created ${newProblems.length} problems.`)
-//     return db.Solution.deleteMany({})
-//   })
-//   .then((_results) => {
-//     console.log("Deleting all solutions...")
-//     return db.Solution.create(solutions)
-//   })
-//   .then((newSolutions) => {
-//     console.log(`Successfully created ${newSolutions.length} solutions.`)
-//     process.exit()
-//   })
-//   .catch((err) => console.log(err))
-
-// {
-//   properties: {
-//     name: "Fatigue",
-//     description:
-//       "Feeling overtired, with low energy and a strong desire to sleep that interferes with normal daily activities.",
-//     image: "../images/brainPain.jpeg",
-//     solutions: [],
-//   },
-//   solutionNames: ["Energy purification", "Crystal"],
-// },
-
-const exampleProblem = problems[0]
 db.Problem.deleteMany({})
   .then((_result) => db.Solution.deleteMany())
   .then(() => db.Solution.create(solutions))
-  .then(() => db.Solution.find({ name: exampleProblem.solutionNames }))
-  .then((results) => {
-    exampleProblem.properties.solutions = results
-    return db.Problem.create(exampleProblem.properties)
+  .then(() =>
+    // return a Promise.all
+    Promise.all(
+      // takes an array of promises
+      // callback of map returns a promise
+      problems.map((problem) =>
+        db.Solution.find({ name: problem.solutionNames }).then((results) => {
+          problem.properties.solutions = results
+          return db.Problem.create(problem.properties)
+        })
+      )
+    )
+  )
+  .then((createdProblems) => {
+    console.log(createdProblems)
+    process.exit()
   })
-  .then((result) => console.log(result))
-
-// //problem 1
-// //find sol by filter name
-// //find by id
-// //insert into proclem solution array
-// //save
